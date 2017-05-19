c2af5fd15ff026a3301e078dcb020a78dfaf34e9
Ginks <pissabruney@hotmail.com>
2017-05-19 18:01:06 +0200

    Mise à jour

--- js/index.js
+++ js/index.js
@@ -2,12 +2,61 @@
  * Created by acer on 05/05/2017.
  */
 console.log("Ce programme JS vient d'être chargé");
+
+
 $(document).ready(function () {
     console.log("Le document est pret");
     var pos=18;
     var posDepartSouris;
     var posDepartBoite;
+    var coups=0;
     var i=0;
+
+    //Gestionnaire d'évenements
+    $('button').click(function () {
+        $('#arriere-plan, #solitaire').css("display","block");
+        //var carte= $('.carte-canvas:first');
+        paquet = shuffle(paquet);
+        for (i=0; i<52; i++) {
+            $('#paquet .placeholder').append(paquet[i]);
+        }
+        var j;
+        //distribution des cartes dans le jeu
+        for(i=8; i!==0; i--){
+            for(j=0; j<i; j++){
+                // carte.animate({left : 10}, {duration : 100});
+
+                $('.carte-canvas:first').css('z-index', j);
+                if(i!==0){
+                    $('.carte-canvas:first').css('top', j*20);
+                }
+                $('#solitaire-plateau').find('.carte').eq(i-1).children('.placeholder').append($('.carte-canvas:first'));
+                console.log(i-1);
+            }
+        }
+
+        $('.carte-canvas').mousedown(function(event)
+        {
+            console.log("Le bouton de la souris a été appuyé sur la boite.");
+            // Seulement le bouton gauche de la souris
+            if(event.which!==1){return;}
+            // Éviter de sélectionner texte si la souris bouge pendant le click
+            event.preventDefault();
+            var boite=$(this);
+            $('.carte-canvas').removeClass('bouge');
+            if(boite.parent().hasClass('deplacable'))
+            {
+                boite.addClass('bouge');
+            }
+            posDepartSouris={left:event.pageX, top: event.pageY};
+            posDepartBoite =boite.offset();
+            console.log($(this).parent());
+        });
+
+
+
+    });
+
     $('#solitaire-plateau .carte').each(function (index) {
         var image = index;
         image=getRandom(2,10);
@@ -15,9 +64,13 @@ $(document).ready(function () {
         $(this).css("left", pos+"%");
         console.log($(this).children());
             pos = pos+8;
+        $(this).children().children().css('position','absolute');
+        $(this).children().addClass('deplacable');
     });
 
-    $('.carte .carte-canvas, #lot .placeholder .carte-canvas').mousedown(function(event)
+
+
+    $('.carte .carte-canvas, #lot .carte-canvas').mousedown(function(event)
     {
         console.log("Le bouton de la souris a été appuyé sur la boite.");
         // Seulement le bouton gauche de la souris
@@ -66,10 +119,10 @@ $(document).ready(function () {
     var lot;
     var n;
         for (lot = 1; lot < 14; lot++) {
-            paquet[lot] = ('<img src="images/' + lot + '_of_spades.png" class="carte-canvas"/>');
-            paquet[lot+14] = ('<img src="images/' + lot + '_of_clubs.png" class="carte-canvas"/>');
-            paquet[lot+28] = ('<img src="images/' + lot + '_of_diamonds.png" class="carte-canvas"/>');
-            paquet[lot+42] = ('<img src="images/' + lot + '_of_hearts.png" class="carte-canvas"/>');
+            paquet[lot] = ('<img src="images/' + lot + '_of_spades.png" class="carte-canvas" style="position: absolute"/>');
+            paquet[lot+14] = ('<img src="images/' + lot + '_of_clubs.png" class="carte-canvas" style="position: absolute"/>');
+            paquet[lot+28] = ('<img src="images/' + lot + '_of_diamonds.png" class="carte-canvas" style="position: absolute"/>');
+            paquet[lot+42] = ('<img src="images/' + lot + '_of_hearts.png" class="carte-canvas" style="position: absolute"/>');
         }
     //Brasser les carte: Code trouvé sur internet
     function shuffle(arr) {
@@ -81,6 +134,10 @@ $(document).ready(function () {
     $('.fermer').mousedown(function () {
             $('#solitaire').fadeOut();
             $('#paquet .placeholder, #lot .placeholder').empty();
+            //supprime les carte de leur emplacement pour une prochaine partie
+            $('#solitaire-plateau .carte').each(function (index) {
+                $('.placeholder').empty();
+            });
 
             //$('#solitaire').css("display","none")
         }
@@ -93,37 +150,26 @@ $(document).ready(function () {
         console.log(cible);
     });
 
-    $('button').click(function () {
-        $('#arriere-plan, #solitaire').css("display","block");
-        //var carte= $('.carte-canvas:first');
-        paquet = shuffle(paquet);
-        for (i=0; i<52; i++) {
-            $('#paquet .placeholder').append(paquet[i]);
-        }
-        var j;
-        //distribution des cartes dans le jeu
-        for(i=8; i!==0; i--){
-            for(j=0; j<i; j++){
-               // carte.animate({left : 10}, {duration : 100});
-                $('#solitaire-plateau').find('.carte').eq(i-1).children('.placeholder').append($('.carte-canvas:first'));
-                console.log(i-1);
-            }
-        }
-        var carteRetour= '<img src="images/black_joker.png" class="carte-canvas">';
-        carteRetour.appendTo('#paquet .placeholder');
-        /**
-        for(i=8; i>0; i--)for (j = 0; j < i; j++) {
-            carte.animate({left: 10}, {duration: 1000});
-            $('#solitaire-plateau .carte').find('.carte').eq(i).append(carte);
-        }
-         **/
-    });
+    //horloge du jeu
+    function horloge(){
+        var hms = new Date();
+        var h, m, s;
+        h = hms.getHours();
+        m = hms.getMinutes();
+        if (m<10) m = "0" + m;
+        s = hms.getSeconds();
+        if (s<10) s = "0" + s;
+        $('#solitaire-tempus').html(h + ' : ' + m + ' : ' + s);
+    }
+    setInterval(horloge,1000);
 
+    //Passage de carte du paquet au lot de carte
     $('.pile #paquet .placeholder:last-child').mousedown(function () {
         var carte= $('.carte-canvas:first');
         carte.animate({top: 10},{duration : 1000});
         console.log(carte);
         $('#lot .placeholder').append(carte);
+        $('#solitaire-coup').text(coups+1);
     });
     console.log("La mise en place est finie. En attente d'événements...");
 });
